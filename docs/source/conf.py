0d49d71cac267ae6cad3533bafecf8865e86753e
Oreste Sciacqualegni <orestesciacqualegni@tutanota.com>
2025-02-22 18:20:12 +0100

    added sphinx plugins

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,15 @@ author = 'Angelo Serrecchia'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.duration'
+]
+coverage_statistics_to_stdout=True
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +31,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
